a7f8514f27aa0277a4871adef3f7a68381d9d651
rithvik-doshi <rithvik@bu.edu>
2023-04-10 09:47:08 -0400

    Consolidated Game Prompt logic

--- src/GameCenter.java
+++ src/GameCenter.java
@@ -1,75 +1,26 @@
 
 import java.io.FileNotFoundException;
-import java.util.*;
 
 /**
  * GameCenter class to instantiate and run Legends-related games
  */
 public class GameCenter {
-    protected LMH_Game lmh_game;
-    protected LOV_Game lov_game;
 
-    public GameCenter() {
-    }
-
-    public void playLMHGame() throws FileNotFoundException {
-        lmh_game.start();
-    }
-
-    public void playLOVGame() throws FileNotFoundException {
-        lov_game.start();
-    }
-
-    public void GamePrompter() {
-        System.out.println(ArtMessages.getWelcomeToGameCenter());
-        System.out.println("Welcome to the Game Center! What game would you like to play?");
-        String printInst = GameEngine.getOption(new String[]{"LMH", "LOV"});
-
-        if (printInst.equals("LMH")) {
-            try {
-                lmh_game = new LMH_Game();
-                playLMHGame();
-                SessionLoop();
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
+    public static void GamePrompter() throws FileNotFoundException {
+        do {
+            System.out.println(ArtMessages.getWelcomeToGameCenter());
+            System.out.println("Which game would you like to play? \n");
+            String option = GameEngine.getOption(new String[]{"Legends of Monsters and Heroes", "Legends Of Valor"});
+            if (option.equals("Legends of Monsters and Heroes")) {
+                LMH_Game game = new LMH_Game();
+                game.start();
+            } else if (option.equals("Legends Of Valor")) {
+                LOV_Game game = new LOV_Game();
+                game.start();
             }
-        }
-        else if (printInst.equals("LOV")) {
-            try {
-                lov_game = new LOV_Game();
-                playLOVGame();
-                SessionLoop();
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-            }
-        }
-        else {
-            System.out.println("Invalid input. Please try again.");
-            GamePrompter();
-        }
-    }
-
-    /**
-     * A Method to allow the user to switch between games
-     */
-    public void SessionLoop() {
-        System.out.println("Do you want to play another game? (Y/N)");
-        String printInst = GameEngine.getOption(new String[]{"Y", "N"});
-        if (printInst.equals("Y")) {
-            GamePrompter();
-        }
-        else if (printInst.equals("N")) {
-            System.out.println("Thank you for playing!");
-        }
-        else {
-            System.out.println("Invalid input. Please try again.");
-            SessionLoop();
-        }
-    }
-
-    public static void main (String[] args) throws FileNotFoundException {
-        GameCenter gameCenter = new GameCenter();
-        gameCenter.GamePrompter();
+            System.out.println("Do you want to play another game? (1 for Y, 2 for N)");
+        } while (GameEngine.getOption(new String[]{"Y", "N"}).equals("Y"));
+        System.out.println("Thanks for playing! Goodbye!");
     }
 
 }
