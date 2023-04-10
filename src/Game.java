9d51d8980f0d44b2f741e08f5f902a73012bfd6a
rithvik-doshi <rithvik@bu.edu>
2023-04-10 09:47:08 -0400

    Consolidated Game Prompt logic

--- src/Game.java
+++ src/Game.java
@@ -1,5 +1,3 @@
-import java.io.FileNotFoundException;
-
 public abstract class Game {
     public abstract void start() throws Exception;
 
@@ -8,21 +6,5 @@ public abstract class Game {
     public abstract boolean checkGameOver();
 
     public abstract void printInstructions();
-
-    public static void gameMenuAndStart() throws FileNotFoundException {
-        do {
-            System.out.println(ArtMessages.getWelcomeToGameCenter());
-            System.out.println("Which game would you like to play? \n");
-            String option = GameEngine.getOption(new String[]{"Legends of Monsters and Heroes", "Legends Of Valor"});
-            if (option.equals("Legends of Monsters and Heroes")) {
-                LMH_Game game = new LMH_Game();
-                game.start();
-            } else if (option.equals("Legends Of Valor")) {
-                LOV_Game game = new LOV_Game();
-                game.start();
-            }
-            System.out.println("Do you want to play another game? (Y/N)");
-        } while (GameEngine.getOption(new String[]{"Y", "N"}).equals("Y"));
-    }
 
 }
